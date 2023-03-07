19c6de64f0906d027850c10d0a0f7094ab499469
Edwin J. Páez <edwinjpr11@gmail.com>
2023-03-07 07:46:58 -0500

    feat(project): create header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, PrimeNgModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
